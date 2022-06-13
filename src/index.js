29049e54256b4617d223fa16be057ba5ec2f64a4
2024kthyagar <karthik6002@gmail.com>
2022-06-13 17:56:51 -0400

    added a header, feel free to change.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+class Heading extends React.Component {
+  render() {
+    return (
+      <div className = "heading">
+        TJHSST Quantum Computing Club
+      </div>
+    )
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Heading />
 );
 
 // If you want to start measuring performance in your app, pass a function
