13bf085f1816b427fcf7270cc37afb29961438e2
william blakc <williamblack7586@gmail.com>
2022-07-17 21:28:07 -0400

    Add footer

--- src/App.js
+++ src/App.js
@@ -2,25 +2,36 @@ import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import './App.css';
 import Header from "./Header.js"
-
+import Footer from "./Footer.js"
 import Home from "./pages/Home"
 import Resources from "./pages/Resources"
+
+import styled from "styled-components"
 
 const App = () => {
     return (
         <Router>
-            <Header />
-            <Routes>
-                <Route path="/" element={
-                    <Home />
-                } />
+            <StyledContainer>
+                <Header />
+                <Routes>
+                    <Route path="/" element={
+                        <Home />
+                    } />
 
-                <Route path="/resources" element={
-                    <Resources />
-                } />
-            </Routes>
+                    <Route path="/resources" element={
+                        <Resources />
+                    } />
+                </Routes>
+                <Footer />
+            </StyledContainer>
         </Router>
     );
 }
+
+const StyledContainer = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+`
 
 export default App;
--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -1,4 +1,5 @@
 import React from "react"
+
 import { PageContainer, PageContentContainer } from "./PageContainer"
 
 const Resources = () => {
